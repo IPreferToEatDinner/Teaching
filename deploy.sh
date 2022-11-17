5f0e097e34eb2cebec65b8795ed49ae4d5ad6fd2
ZSR <2472990740@qq.com>
2022-11-17 20:41:02 +0800

    更新了deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # 忽略错误
 set -e
 
-# 进行构建
+# 构建
 npm run docs:build
 
 # 进入待发布的目录
@@ -14,11 +14,14 @@ sed -i '5 i <meta name="msvalidate.01" content="C246CD2DBA296401E59B4D6C4A608161
 sed -i '5 i <meta name="baidu-site-verification" content="code-ZoEDd8If6G" />' index.html
 
 
-git add .
+git init
+git add -A
 git commit -m 'deploy'
-# git remote remove origin
-git remote add git@github.com:IPreferToEatDinner/iprefertoeatdinner.github.io.git
-git push -u origin master
 
+# 如果部署到 https://<USERNAME>.github.io
+git push -f git@github.com:IPreferToEatDinner/iprefertoeatdinner.github.io.git master
+
+# 如果是部署到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
