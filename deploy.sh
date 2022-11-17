f508722a95228dc717745b95eae97a812d476234
ZSR <2472990740@qq.com>
2022-11-17 20:40:52 +0800

    完全更新了deploy，千万别动，动了就会出现git版本问题

--- deploy.sh
+++ deploy.sh
@@ -9,12 +9,15 @@ npm run docs:build
 # 进入待发布的目录
 cd docs/.vitepress/dist
 
+# 加入文件标识头
+sed -i '5 i <meta name="msvalidate.01" content="C246CD2DBA296401E59B4D6C4A608161" />' index.html
+sed -i '5 i <meta name="baidu-site-verification" content="code-ZoEDd8If6G" />' index.html
+
 
 git add .
 git commit -m 'deploy'
-
-
-# 如果部署到 https://<USERNAME>.github.io
+# git remote remove origin
+git remote add git@github.com:IPreferToEatDinner/iprefertoeatdinner.github.io.git
 git push -u origin master
 
 
